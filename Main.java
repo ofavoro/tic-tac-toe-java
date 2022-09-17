e4abb5561ed39ddd729248c2702d022c94da73a8
favorxo <favorxog@gmail.com>
2022-09-17 21:40:44 +0300

    feat: init

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    int a = 0;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ TicTacToe.java
@@ -0,0 +1,25 @@
+enum Move {
+    X,
+    O,
+    I
+}
+
+public class TicTacToe {
+    Move[][] board = { { Move.I, Move.I, Move.I }, { Move.I, Move.I, Move.I }, { Move.I, Move.I, Move.I } };
+
+    public void gameLoop() {
+
+    }
+
+    private boolean checkWin() {
+        return false;
+    }
+
+    private void makeMove() {
+
+    }
+
+    private int getInput() {
+        return 0;
+    }
+}
