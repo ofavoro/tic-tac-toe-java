d0435013430e1603290c64a9b8bf03d7e7f562e9
favorxo <favorxog@gmail.com>
2022-09-17 21:46:19 +0300

    feat: check win function

--- TicTacToe.java
+++ TicTacToe.java
@@ -12,6 +12,26 @@ public class TicTacToe {
     }
 
     private boolean checkWin() {
+        for (int y = 0; y < 3; y++) {
+            if (board[y][0] == board[y][1] && board[y][1] == board[y][2] && board[y][0] != Move.I) {
+                return true;
+            }
+        }
+
+        for (int x = 0; x < 3; x++) {
+            if (board[0][x] == board[1][x] && board[1][x] == board[2][x] && board[0][x] != Move.I) {
+                return true;
+            }
+        }
+
+        if (board[0][0] != Move.I && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
+            return true;
+        }
+
+        if (board[2][0] != Move.I && board[2][0] == board[1][1] && board[1][1] == board[0][2]) {
+            return true;
+        }
+
         return false;
     }
 
